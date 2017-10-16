dd5027aff9b70b3acf895dbed3d16074cd6d2674
macbook pro yifan <yifan@magicmoma.com>
2017-10-16 16:36:26 +0800

    init 006, add snapkit

---
+++ 006-timer/006-timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  006-timer
+//
+//  Created by  moma on 2017/10/16.
+//  Copyright © 2017年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
