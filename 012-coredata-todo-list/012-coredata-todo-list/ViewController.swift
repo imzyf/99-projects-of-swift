cec3550df6bfdd8e488742dac72d214ec63add4d
macbook pro yifan <yifan@magicmoma.com>
2017-10-30 17:15:24 +0800

    012 init

---
+++ 012-coredata-todo-list/012-coredata-todo-list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  012-coredata-todo-list
+//
+//  Created by  moma on 2017/10/30.
+//  Copyright © 2017年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
