40d5b81aea93e9afd4cf97103eaf9a02b7f62edc
macbook pro yifan <yifan@magicmoma.com>
2017-10-30 19:11:52 +0800

    012 finish coding

--- 012-coredata-todo-list/012-coredata-todo-list/AppDelegate.swift
+++ 012-coredata-todo-list/012-coredata-todo-list/AppDelegate.swift
@@ -14,9 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let dictionary: [String: Any] = ["ChecklistItemID": 0]
+        UserDefaults.standard.register(defaults: dictionary)
+        
         return true
     }
 
@@ -58,7 +61,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
--- 012-coredata-todo-list/012-coredata-todo-list/ViewController.swift
+++ 012-coredata-todo-list/012-coredata-todo-list/ViewController.swift
@@ -7,14 +7,106 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,
+UITableViewDelegate, UITableViewDataSource {
+ 
+    @IBOutlet weak var todoTableView: UITableView!
+ 
+    var datas = [TodoItem]()
+    let entityName = "TodoItem"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        todoTableView.delegate = self
+        todoTableView.dataSource = self
+        
+        self.getAllTodoItems()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "item")
+        
+        let data = datas[indexPath.row]
+        cell.textLabel?.text = "\(data.id) - \(data.title!)" 
+        cell.detailTextLabel?.text = data.subTitle
+        
+        return cell
+    }
+    
+    @IBAction func addTodoItem() {
+        let alertController = UIAlertController(title: "New Todo Item", message: "", preferredStyle:.alert)
+  
+        // add action
+        alertController.addAction(UIAlertAction(title: "Add", style: .default) { (_) in
+            if let fields = alertController.textFields  {
+                self.storeItem(title: fields[0].text!, subTitle: fields[1].text!)
+                self.getAllTodoItems()
+                self.todoTableView.reloadData()
+            }
+        })
+        // cancel action
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Todo Item Title"
+        }
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Todo Item SubTitle"
+        }
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK:- CoreData
+    func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    func storeItem(title:String, subTitle:String) {
+        let context = getContext()
+        // 定义一个entity，这个entity一定要在xcdatamodeld中做好定义
+        let entity = NSEntityDescription.entity(forEntityName: entityName, in: context)
+        
+        let item = NSManagedObject(entity: entity!, insertInto: context)
+        item.setValue(title, forKey: "title")
+        item.setValue(subTitle, forKey: "subTitle")
+        item.setValue(ViewController.nextChecklistItemID(), forKey: "id")
+      
+        do {
+            try context.save()
+            print("saved")
+        }catch{
+            print(error)
+        }
+    }
+    
+    // 获取某一entity的所有数据
+    func getAllTodoItems() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        do {
+            let searchResults = try getContext().fetch(fetchRequest)
+            datas = searchResults as! [TodoItem]
+        } catch  {
+            print(error)
+        }
+    }
+    
+    class func nextChecklistItemID() -> Int {
+        let userDefaults = UserDefaults.standard
+        let itemID = userDefaults.integer(forKey: "ChecklistItemID")
+        userDefaults.set(itemID + 1, forKey: "ChecklistItemID")
+        userDefaults.synchronize()
+        return itemID
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
