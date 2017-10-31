07f84531ca3c7bdf2e4cd0b562995c8852ca7bd4
macbook pro yifan <yifan@magicmoma.com>
2017-10-31 10:09:14 +0800

    014 init

--- 013-simple-photo-browser/README.md
+++ 013-simple-photo-browser/README.md
@@ -8,6 +8,7 @@
 图片缩放。
 
 1. `UIScrollViewDelegate` `viewForZooming(in scrollView: UIScrollView)`。
+2. iOS 模拟器中按 `shift + option` 两点触控。
 
 ## Reference
 - [抓住iOS的未来 - 30天学习编写30个Swift小程序 - 7](http://www.jianshu.com/p/c6ae28964ad5)
---
+++ 014-video-splash/014-video-splash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  014-video-splash
+//
+//  Created by  moma on 2017/10/31.
+//  Copyright © 2017年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
