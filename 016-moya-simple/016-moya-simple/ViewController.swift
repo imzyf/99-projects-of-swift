67acb6248d8c697c1612070e6dec7ba0a446e987
macbook pro yifan <yifan@magicmoma.com>
2017-11-02 19:41:08 +0800

    016 init

---
+++ 016-moya-simple/016-moya-simple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  016-moya-simple
+//
+//  Created by  moma on 2017/11/2.
+//  Copyright © 2017年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
