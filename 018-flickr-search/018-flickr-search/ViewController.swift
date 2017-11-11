d25a510f190c9125c7f7dd47bbadc3ca0e3acc78
macbook pro yifan <yifan@magicmoma.com>
2017-11-11 16:06:10 +0800

    018 init

---
+++ 018-flickr-search/018-flickr-search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  018-flickr-search
+//
+//  Created by  moma on 2017/11/11.
+//  Copyright © 2017年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
