db803bc3901fb5cd21e18eefdd2142c263b9ea37
macbook pro yifan <yifan@magicmoma.com>
2017-11-18 17:15:26 +0800

    019 init

---
+++ 019-twitter-bird/019-twitter-bird/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  019-twitter-bird
+//
+//  Created by  moma on 2017/11/18.
+//  Copyright © 2017年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
