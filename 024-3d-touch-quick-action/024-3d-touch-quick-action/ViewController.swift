331e5a71391dc7857f84febdaa174d6ce685b4e5
macbook pro yifan <yifan@magicmoma.com>
2018-02-09 15:06:41 +0800

    024 init

---
+++ 024-3d-touch-quick-action/024-3d-touch-quick-action/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  024-3d-touch-quick-action
+//
+//  Created by  moma on 2018/2/9.
+//  Copyright © 2018年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
