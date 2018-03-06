9d18e27e2da2c25ec2e2bfe036361cc365d84cb5
macbook pro yifan <yifan@magicmoma.com>
2018-03-06 21:16:11 +0800

    task: 026 init

---
+++ 026-safe-area/026-safe-area/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  026-safe-area
+//
+//  Created by  moma on 2018/3/6.
+//  Copyright © 2018年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
