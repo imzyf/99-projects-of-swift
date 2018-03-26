56273aecc2f5abbb3385e495b041ebc3caaf78ed
macbook pro yifan <yifan@magicmoma.com>
2018-03-26 15:27:06 +0800

    task: init 028

---
+++ 028-circle-progress/028-circle-progress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  028-circle-progress
+//
+//  Created by  moma on 2018/3/26.
+//  Copyright © 2018年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
