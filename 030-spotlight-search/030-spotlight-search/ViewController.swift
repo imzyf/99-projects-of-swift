b639916be4ce37a629d88fe6e14af6f30023ee16
macbook pro yifan <yifan@magicmoma.com>
2018-03-27 17:17:55 +0800

    feat: init 030 spotlight search

---
+++ 030-spotlight-search/030-spotlight-search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  030-spotlight-search
+//
+//  Created by  moma on 2018/3/27.
+//  Copyright © 2018年 yifans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
